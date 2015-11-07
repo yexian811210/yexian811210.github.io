45f0a5cd6432a1df195102db612e8184dd82002c
王力涵 <lileewang111@gmail.com>
2015-11-07 01:27:32 -0500

    initial commit

---
+++ ToDoList/register.php
@@ -0,0 +1,20 @@
+<?php
+   if($_SERVER["REQUEST_METHOD"] == "POST") {
+   	$username = mysql_real_escape_string($_POST['email_address']);
+   	$password = mysql_real_escape_string($_POST['password']);
+    mysql_connect("localhost", "root","") or die(mysql_error());
+    mysql_select_db("first_db") or die("Cannot connect to database");
+    $query = mysql_query("SELECT * FROM users");
+    while($user_information = mysql_fetch_array($query)) {
+    	$username_from_table = $user_information['username'];
+    	if($username == $username_from_table) {
+    		Print '<script>alert(email address is taken!)</script>';
+    	}
+     }
+     mysql_query("INSERT INTO users (username, password) VALUES ('$username', '$password')");
+     Print '<script>alert("Successfully registered!")</script>';
+   }
+   else {
+   	header("location: index.html");
+   }
+?>
